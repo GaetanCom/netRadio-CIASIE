1415cb2726b190761a0f5184f11e1f982489a8e0
GaetanCom <comacle.gaetan@gmail.com>
2021-02-15 17:59:42 +0100

    tets

---
+++ frontend/src/container/Index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Index extends Component {
+
+    render() {
+
+        return(
+
+            <div>
+                <div>
+                    <Title text={""} />
+                    <Subtitle />
+                </div>
+            </div>
+
+        );
+
+    }
+
+}
+
+export default Index;
